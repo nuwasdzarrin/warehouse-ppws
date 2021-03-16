c44be87fb1777a2b520ca674e37e638dc2dd3b5d
Nuwas Dzarrin <nuwasdzarrin3@gmail.com>
2021-03-16 14:10:15 +0700

    update trasaction report

--- app/Http/Controllers/TransactionReportController.php
+++ app/Http/Controllers/TransactionReportController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Transaction;
 use Illuminate\Http\RedirectResponse;
-use mikehaertl\wkhtmlto\Pdf;
+//use mikehaertl\wkhtmlto\Pdf;
 use Illuminate\Database\Eloquent\Builder;
-//use Barryvdh\Snappy\Facades\SnappyPdf as PDF;
+use Barryvdh\Snappy\Facades\SnappyPdf as PDF;
 
 /**
  * TransactionReportController
@@ -28,44 +28,37 @@ class TransactionReportController extends Controller
      *
      * @return \Illuminate\Http\Response|RedirectResponse
      */
-    public function __invoke()
+    public function index()
     {
-        // TODO: Controller logic
         $institution_id = request()->cookie('institution_id');
 
         $transactions = Transaction::query()->when(auth()->user()->hasRole(['superadmin']), function (Builder $query) use ($institution_id) {
             return $query->where($query->qualifyColumn('institution_id'), $institution_id);
         })->filter()->paginate()->appends(request()->query());
 
-        if (request()->exists('pdf')) {
-            $render = view('prints.transaction_report', [ 'transactions' => $transactions ])->render();
-
-            $pdf = new Pdf;
-            $pdf->addPage($render);
-            $pdf->setOptions(['javascript-delay' => 5000]);
-
-            $pdf->saveAs(public_path('reports/transactions/Laporan Transaksi.pdf'));
-
-            return response()->download(public_path('reports/transactions/Laporan Transaksi.pdf'));
-
-//            $pdf->send('Laporan Transaksi '.\Carbon\Carbon::now()->format('d-m-Y Hi').'.pdf');
-//            return redirect()->route('transaction_report');
-
-//            return PDF::loadView('prints.transaction_report', [ 'transactions' => $transactions ])
-//                ->setPaper('a3')
-//                ->setOption('margin-bottom', 10)
-//                ->setOption('margin-top', 11)
-//                ->setOption('enable-javascript', true)
-//                ->setOption('javascript-delay', 5000)
-//                ->setOption('title', "Cetak Laporan Transaksi")
-//                ->inline("cetak-laporan-tarnsaksi.pdf");
-        }
-
-//        script for debugging
-        if (request()->exists('dev')) return response()->view('prints.transaction_report', [ 'transactions' => $transactions ]);
-
         return response()->view('transaction_report', [
             'transactions' => $transactions
         ]);
+    }
+
+    public function print()
+    {
+        $institution_id = request()->cookie('institution_id');
+
+        $transactions = Transaction::query()->when(auth()->user()->hasRole(['superadmin']), function (Builder $query) use ($institution_id) {
+            return $query->where($query->qualifyColumn('institution_id'), $institution_id);
+        })->filter()->paginate()->appends(request()->query());
+
+        return PDF::loadView('prints.transaction_report', [ 'transactions' => $transactions, 'chartImg' => request()->chartImg ])
+            ->setPaper('a3')
+            ->setOption('margin-bottom', 10)
+            ->setOption('margin-top', 11)
+            ->setOption('enable-javascript', true)
+            ->setOption('javascript-delay', 5000)
+            ->setOption('title', "Cetak Laporan Transaksi")
+            ->inline("cetak-laporan-transaksi.pdf");
+
+//        script for debugging
+//        if (request()->exists('dev')) return response()->view('prints.transaction_report', [ 'transactions' => $transactions ]);
     }
 }
--- resources/views/transaction_report.blade.php
+++ resources/views/transaction_report.blade.php
@@ -3,6 +3,36 @@
 @section('content-title', ucwords(__('transaction_report.plural')))
 
 @section('content')
+    @php
+        /** @var \App\Transaction $transactions */
+        $transaction_in = $transactions->map(function ($transaction) {
+            return collect([
+                'key' => $transaction->created_at->format('d M, h:s'),
+                'value' => $transaction->in_out=='in'?((int)$transaction->amount):0
+                ]);
+        });
+        $transaction_out = $transactions->map(function ($transaction) {
+            return collect([
+                'key' => $transaction->created_at->format('d M, h:s'),
+                'value' => $transaction->in_out=='out'?$transaction->amount:0
+                ]);
+        });
+        $stock= [];
+        if (request()->product_id) {
+            $temp = 0;
+            foreach ($transactions as $key => $transaction) {
+                if ($transaction->in_out=='in') {
+                    $temp += $transaction->amount;
+                } else {
+                    $temp -= $transaction->amount;
+                }
+                array_push($stock, collect([
+                    'key' => $transaction->created_at->format('d M, h:s'),
+                    'value' => $temp
+                ]));
+            }
+        }
+    @endphp
     <div class="row">
         <div class="col-md-12">
             <div class="ibox float-e-margins">
@@ -17,6 +47,127 @@
             </div>
         </div>
     </div>
+{{--    chart display none for chartImg--}}
+    <div>
+        <div id="my_dataviz" style="display: none"></div>
+    </div>
+    <script>
+        let dataset = @json($transaction_out);
+        let dataset_in = @json($transaction_in);
+        let dataset_stock = @json($stock);
+        // set the dimensions and margins of the graph
+        let margin = {
+                top: 20,
+                right: 20,
+                bottom: 120,
+                left: 50
+            },
+            width = 600 - margin.left - margin.right,
+            height = 400 - margin.top - margin.bottom;
+
+        // set the ranges
+        let x = d3.scalePoint().range([0, width]);
+        let y = d3.scaleLinear().range([height, 0]);
+
+        // define the line
+        let valueline = d3.line()
+            .x(function(d) {
+                return x(d.key);
+            })
+            .y(function(d) {
+                return y(d.value);
+            });
+        // append the svg obgect to the body of the page
+        // appends a 'group' element to 'svg'
+        // moves the 'group' element to the top left margin
+        let svg = d3.select("#my_dataviz")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+
+        function maxValue(arr) {
+            let max = arr[0];
+            for (let val of arr) {
+                if (val > max) {
+                    max = val;
+                }
+            }
+            return max;
+        }
+
+        function findMaxYAxis(data, data1, data2) {
+            let maxY = [];
+            maxY[0] = d3.max(data, function(d) {
+                return d.value;
+            });
+            maxY[1] = d3.max(data1, function(d) {
+                return d.value;
+            });
+            maxY[2] = d3.max(data2, function(d) {
+                return d.value;
+            });
+            return maxValue(maxY);
+        }
+
+        function draw(data, data1, data2) {
+
+            data.forEach(function(d) {
+                d.value = +d.value;
+            });
+
+            // Scale the range of the data
+            x.domain(data.map(function(d) {
+                return d.key;
+            }));
+            y.domain([0, findMaxYAxis(data, data1, data2)]);
+
+            // Add the valueline path.
+            svg.append("path")
+                .datum(data)
+                // .attr("class", "line-out")
+                .attr("fill", "none")
+                .attr("stroke", "rgb(165,54,54)")
+                .attr("stroke-width", 3)
+                .attr("d", valueline);
+
+            //dataset2
+            svg.append("path")
+                .datum(data1)
+                // .attr("class", "line-in")
+                .attr("fill", "none")
+                .attr("stroke", "rgba(26,179,148,0.7)")
+                .attr("stroke-width", 3)
+                .attr("d", valueline);
+
+            if (data2) {
+                //dataset3
+                svg.append("path")
+                    .datum(data2)
+                    // .attr("class", "line-stock")
+                    .attr("fill", "none")
+                    .attr("stroke", "rgba(155,201,196,0.7)")
+                    .attr("stroke-width", 3)
+                    .attr("d", valueline);
+            }
+
+            svg.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x))
+                .selectAll("text")
+                .attr("transform", "translate(-10,10)rotate(-45)")
+                .style("text-anchor", "end");
+
+            // Add the Y Axis
+            svg.append("g")
+                .call(d3.axisLeft(y));
+        }
+        draw(dataset, dataset_in, dataset_stock);
+    </script>
+{{--    end of chart display none for chartImg--}}
+
     <div class="row">
       <div class="col-md-12">
         @component(config('generator.view_component').'components.panel')
@@ -25,9 +176,19 @@
           @endslot
           @slot('tools')
               <div class="ibox-tools">
-                  <a href="{{ route('transaction_report', ['pdf']) }}" class="btn btn-primary btn-sm">
-                      <i class="fa fa-print"></i> Cetak Laporan
-                  </a>
+                  <form action="{{ route('transaction_report.print') }}" method="POST">
+                      @csrf
+                      <div class="form-group">
+                          <input type="hidden" name="pdf" value="true">
+                          <input type="hidden" name="chartImg" id="chartImg">
+                          <button class="btn btn-primary btn-sm" type="submit"><i class="fa fa-print"></i> Cetak Laporan</button>
+                      </div>
+                  </form>
+                  <script type="text/javascript">
+                      var s = new XMLSerializer().serializeToString(document.getElementById("my_dataviz").firstChild);
+                      var encodedData = 'data:image/svg+xml;base64,' + window.btoa(s);
+                      document.getElementById("chartImg").value = encodedData;
+                  </script>
               </div>
           @endslot
 
@@ -133,6 +294,9 @@
       </div>
     </div>
 @endsection
+@push('head')
+    <script src="https://d3js.org/d3.v4.min.js"></script>
+@endpush
 @push('body')
     <!-- ChartJS-->
     <script src="js/plugins/chartJs/Chart.min.js"></script>
